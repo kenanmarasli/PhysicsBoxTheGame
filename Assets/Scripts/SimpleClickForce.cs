9efdb78286da5beb72682c96aec7c9389d2e576f
Kenan Marasli <kenan.marasli@kodobur.com>
2020-08-31 15:31:29 +0300

    added chains

---
+++ Assets/Scripts/SimpleClickForce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleClickForce : ClickablePhysicsObject
+{
+    [SerializeField]
+    private float forceMagnitude = 2000.0f;
+
+    public override void Click(ClickEvent cEvent)
+    {
+        base.Click(cEvent);
+        rigidbodyComp.AddForceAtPosition(cEvent.direction * forceMagnitude, cEvent.position);
+    }
+}
